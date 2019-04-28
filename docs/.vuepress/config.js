36b170d7356051b2e50d581251d037baa5708d80
yuanda <1754906214@qq.com>
2019-04-28 10:01:21 +0800

    ...

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const utils = require('./utils')
 module.exports = {
   title: 'Learn - More',
   description: '个人站',
-  // base: '/awesome-bookmarks/',
+  base: '/learnmore/',
   head: [
     [
       'link',
@@ -14,36 +14,32 @@ module.exports = {
     ]
   ],
   themeConfig: {
-    '/': {
-      label: '简体中文',
-      repo: 'guhuilin/study',
-      nav: [
-        {
-          text: '首页',
-          link: '/'
-        },
-        {
-          text: '学习链接',
-          link: '/repository/'
-        },
-        {
-          text: '笔记整理',
-          link: '/website/'
-        },
-        {
-          text: '文章',
-          link: '/article/'
-        },
-        {
-          text: 'preview',
-          link: '/interview/'
-        },
-        {
-          text: '杂记',
-          link: '/blog/'
-        }
-      ]
-    },
+    nav: [
+      {
+        text: '首页',
+        link: '/'
+      },
+      {
+        text: '学习链接',
+        link: '/repository/'
+      },
+      {
+        text: '笔记整理',
+        link: '/website/'
+      },
+      {
+        text: '文章',
+        link: '/article/'
+      },
+      {
+        text: 'preview',
+        link: '/interview/'
+      },
+      {
+        text: '杂记',
+        link: '/blog/'
+      }
+    ],
     sidebar: utils.inferSiderbars(),
     lastUpdated: '上次更新',
     repo: 'guhuilin/learnmore',
